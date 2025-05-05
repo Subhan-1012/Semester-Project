832e8167d3af69f1c7400bbc3ae126607004c8f7
Subhan-1012 <pubgsquad302@gmail.com>
2025-05-05 17:46:09 +0500

    Added economy class

---
+++ economy.h
@@ -0,0 +1,17 @@
+#pragma once
+#include"population.h"
+class Economy {
+    private:
+        int treasury;
+        float taxRate;
+        float inflation;
+    public:
+        Economy();
+        void taxPopulation(const Population& pop);
+        void spend(int amount);
+        void showStats() const;
+        void saveToFile() const;
+        void loadFromFile();
+        int getTreasury() const;
+        void receiveLoan(int amount);
+    };
